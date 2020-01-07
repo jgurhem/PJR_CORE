bcaede41f312f44c7fe77216208ffc8ecc49f4fa
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-01-07 22:01:47 +0100

    Add subcase possibility and retrieve several statics from the database

--- DBRelator.py
+++ DBRelator.py
@@ -1,4 +1,21 @@
-def matrix_relation(con, list_cases, case_of_interest, value_of_interest, relname, op):
+import numpy as np
+
+def matrix_relation(con, list_cases, list_sub_cases, case_of_interest, value_of_interest, relname, stats):
+  """ Gather data from the database in order to use them in a plot.
+  The list of sub cases (list_sub_cases) is used to select the best case formed with the main case and sub case parameters
+  using the minimum value of the first statitic put in the stats variable.
+
+  Function parameters:
+
+  con                --- connection to the sqlite3 database
+  list_cases         --- list of parameters to consider as main cases
+  list_sub_cases     --- list of parameters to consider as sub cases, can be empty
+  case_of_interest   --- parameter to compare to the main cases
+  value_of_interest  --- parameter which values will be used as comparison
+  relname            --- prefix of the tables used to retrieve data
+  stats              --- list of statistics to retrieve from the database
+  """
+
   cur = con.cursor()
 
   query = f'SELECT DISTINCT {case_of_interest} FROM {relname}_cases'
@@ -18,14 +35,24 @@ def matrix_relation(con, list_cases, case_of_interest, value_of_interest, relnam
   for r in rows:
     m[r] = dict()
     for c in columns:
-      query = f'SELECT {op} FROM {relname}_{value_of_interest}_stats INNER JOIN {relname}_cases ON {relname}_{value_of_interest}_stats.rowid={relname}_cases.rowid WHERE '
+      query = f'SELECT '
+      for i in stats:
+        query += f'{relname}_{value_of_interest}_stats.' + i + ','
+      query = query.rstrip(',')
+      for i in list_sub_cases:
+        query += f',{relname}_cases.' + i
+      query += f' FROM {relname}_{value_of_interest}_stats INNER JOIN {relname}_cases ON {relname}_{value_of_interest}_stats.rowid={relname}_cases.rowid WHERE '
       query += f'{case_of_interest}={c}'
       for i in range(len(list_cases)):
         query += f' AND {relname}_cases.' + list_cases[i] + "='" + str(r[i]) + "'"
       cur.execute(query)
-      res = cur.fetchone()
+      res = cur.fetchall()
+      min_pos = np.argmin([float(x[0]) for x in res])
       if res != None:
-        m[r][c] = res[0]
+        for i in range(len(stats)):
+          m[r][str(c) + '_' + stats[i]] = res[min_pos][i]
+        for i in range(len(list_sub_cases)):
+          m[r][str(c) + '_' + list_sub_cases[i]] = res[min_pos][len(stats) + i]
       else:
         m[r][c] = None
   return m, columns
